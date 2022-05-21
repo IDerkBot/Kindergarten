1697df838f1e225ad1afbdafb459cb5eef0cdf71
IDerkBot <ivanderk2003@gmail.com>
2022-05-21 17:08:43 +0300

    Добавил классы

---
+++ Models/Data.cs
@@ -0,0 +1,11 @@
+﻿namespace Kindergarten.Models
+{
+	internal class Data
+	{
+		public static int Access { get; set; }
+		public static int IDUser { get; set; }
+		public static bool IsParent => Access == 0;
+		public static bool IsTeacher => Access == 1;
+		public static bool IsAdmin => Access == 2;
+	}
+}
---
+++ Models/PageManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using Kindergarten.Views.Windows;
+using MaterialDesignThemes.Wpf;
+using System.Timers;
+
+namespace Kindergarten.Models
+{
+	internal class PageManager
+	{
+		private static Frame MainFrame { get; set; }
+        public static void SetFrame(Frame frame) { MainFrame = frame; }
+		public static void Navigate(Page titlePage) => MainFrame.Navigate(titlePage);
+		public static void GoBack() { if (MainFrame.CanGoBack) MainFrame.GoBack(); }
+        public static bool CanGoBack() => MainFrame.CanGoBack;
+    }
+
+    internal class AlertManager
+    {
+        private static MainWindow MainWindow { get; set; }
+        public static void SetWindow(MainWindow window)
+        {
+            MainWindow = window;
+        }
+        public static void Alert(string message)
+        {
+            MainWindow.SbAlert.Message = new SnackbarMessage { Content = message };
+            MainWindow.SbAlert.IsActive = true;
+            MainWindow.MainTimer.Start();
+        }
+    }
+}
