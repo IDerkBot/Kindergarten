321438b4e80c2639f505d98fe0220107b550a8a5
IDerkBot <ivanderk2003@gmail.com>
2022-05-21 17:08:55 +0300

    Пересестил окно

---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using Kindergarten.Models;
+
+namespace Kindergarten.Views.Windows
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+    {
+        public Timer MainTimer { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PageManager.SetFrame(MainFrame);
+            PageManager.Navigate(new AuthPage());
+            AlertManager.SetWindow(this);
+            MainTimer = new Timer(3000);
+            MainTimer.Elapsed += MainTimerOnElapsed;
+        }
+
+        private void MainTimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                SbAlert.IsActive = false;
+                MainTimer.Stop();
+            });
+        }
+
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            BtnBack.Visibility = PageManager.CanGoBack() ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PageManager.GoBack();
+        }
+    }
+}
